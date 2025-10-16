81c4301f7ed8c21f0ead4dbbf80a86c24610c018
calista0314 <crista0314@foxmail.com>
2025-10-16 15:33:45 +0800

    20251016

--- tools/trip_ebs.py
+++ tools/trip_ebs.py
@@ -4,6 +4,7 @@ from typing import Any
 from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
 
+import json
 import pandas as pd
 from io import StringIO, BytesIO
 
@@ -11,7 +12,16 @@ class TripEbsTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
         json_str = tool_parameters['json_str']
         try:
-            df = pd.read_json(StringIO(json_str), dtype=str)
+            data = json.loads(json_str)
+            
+            cols = list(dict.fromkeys(j for i in data for j in i))
+            
+            required =['公司段编码', '科目段编码', '子目段编码', '部门段编码', 'BU段编码', '产品分类段编码', '往来段编码', 'BU往来段编码', '项目段编码', '备用段1编码', '备用段2编码']
+
+            cols_str = set(cols) & set(required)
+            
+            df = pd.read_json(StringIO(json_str), dtype= {i: str for i in cols_str})
+            
         except Exception as e:
             raise Exception(f"Error reading JSON string: {str(e)}")
           
@@ -23,7 +33,6 @@ class TripEbsTool(Tool):
             required5 = ['子目段编码', 'BU段编码', 'BU往来段编码']
 
             required6 = ['往来段编码', '项目段编码', '备用段1编码', '备用段2编码']
-
 
             ebs_template = ['序号', '分类账', '类别', '批名', '期间', '日记帐名称', '日记账说明', '有效日期', '币种', '汇率类型',
                         '汇率日期', '汇率', '审批人', '公司段编码', '公司说明', '科目段编码', '科目段说明', '子目段编码',
@@ -42,7 +51,13 @@ class TripEbsTool(Tool):
         
                     mapping[i] = matches[0]
                     
-                else:
+            for i, j in mapping.items():
+            
+                df_ebs_template[i] = df[j]
+                
+            cols_required = set(required) - set(cols)
+            
+            for i in cols_required:    
                     
                     if i in required4:
                         
@@ -55,19 +70,10 @@ class TripEbsTool(Tool):
                     elif i in required6:
                         
                         df_ebs_template[i] = '000000'
-                        
-                        
-            for i, j in mapping.items():
-            
-                df_ebs_template[i] = df[j]
-                
+                                                
             df_ebs_template['序号'] = range(1, len(df_ebs_template)+1)
             
-            df_ebs_template['类别'] = '记账'
-            
-            df_ebs_template['原币借方金额'] = pd.to_numeric(df_ebs_template['原币借方金额'], errors='coerce').round(2)
-
-            df_ebs_template['原币贷方金额'] = pd.to_numeric(df_ebs_template['原币贷方金额'], errors='coerce').round(2)
+            df_ebs_template['类别'] = df_ebs_template['类别'].fillna('记账')
             
         except Exception as e:
             raise Exception(f"Error generating EBS templates: {str(e)}")
